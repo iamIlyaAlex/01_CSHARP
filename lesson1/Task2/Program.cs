6a070ad26744cfec8419a9f6707b98ab45962146
“iamIlyaAlex” <blagas77@me.com>
2023-12-21 02:52:23 +0300

    lesson1:task2

---
+++ lesson1/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int i1 = 5; // 5
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 2 * 10; // 20
+long sum2 = l1 + i1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5; // 5.5
+double sum3 = d1 + i1;
+Console.WriteLine(sum3);
+
+int product1 = i1 * i1; // 5 * 5 = 25
+long product2 = l1 * i1; // (10 * 2) * 5 = 100 
+double product3 = i1 * d1;  // 5 * 5,5 = 27,5
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
